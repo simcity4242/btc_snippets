9d226be5ce8070904bccc7d4d59a906d28445831
wizard of ozzie <simcity4242@users.noreply.github.com>
2015-06-04 09:23:32 +1000

    Update pyscrypt1.py
    merged py2/3 funcs

--- pyscrypt1.py
+++ pyscrypt1.py
@@ -1,25 +1,4 @@
-# The MIT License (MIT)
-#
-# Copyright (c) 2014 Richard Moore
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
+# https://github.com/ricmoo/pyscrypt/blob/master/pyscrypt/hash.py
 
 import hashlib
 import hmac
@@ -52,6 +31,18 @@ else:
     def chars_to_bytes(array):
         return bytes(array)
 
+# def get_byte(c):
+#     return ord(c) if str==bytes else c
+#
+# def chars_to_bytes(array):
+#     if str==bytes:
+#         return ''.join(chr(c) for c in array)
+#     return bytes(array)
+#
+# def check_bytes(byte_array):
+#     if not str==bytes:
+#         return isinstance(byte_array, bytes)
+#     return True
 
 def pbkdf2_single(password, salt, key_length, prf):
     '''Returns the result of the Password-Based Key Derivation Function 2 with
